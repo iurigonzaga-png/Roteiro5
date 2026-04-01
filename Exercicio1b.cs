eb6fe94b99ba810f5ac201697a64f7a519bc5e08
iurigonzaga-png <iuri.gonzaga@esamcuberlandia.com.br>
2026-04-01 20:15:04 -0300

    Implement investment return calculation program
    Add a program to calculate investment returns based on initial capital, monthly interest, and investment duration.

--- Exercicio1b.cs
+++ Exercicio1b.cs
@@ -1,1 +1,20 @@
+using System;
+    class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("informe seu capital inicial");
+       string cinicial = Console.ReadLine();
+        Console.WriteLine("informe o juros mensal");
+       string jmensal = Console.ReadLine();
+        Console.WriteLine("informe quantos meses ficara investido");
+       string minv = Console.ReadLine();
+        double capitalInicial = Convert.ToDouble(cinicial);
+        double juros = Convert.ToDouble(jmensal);
+        double meses = Convert.ToDouble(minv);
 
+        double montante = capitalInicial * Math.Pow(1 + juros, meses);
+        double lucro = montante - capitalInicial;
+        double lucroArredondado = Math.Round(lucro, 2);
+
+    } }
